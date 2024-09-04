8cd111337cb402b73e53d298bc3a69a340a14fdd
gnmathew <genemathewdorado@gmail.com>
2024-09-04 17:53:19 +0800

    [Add] id attribute in posts_serializer

--- app/serializers/api/v1/posts_serializer.rb
+++ app/serializers/api/v1/posts_serializer.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class PostsSerializer
       include FastJsonapi::ObjectSerializer
-      attributes :title, :description
+      attributes :id, :title, :description
 
     end
   end
